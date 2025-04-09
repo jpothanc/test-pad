0d73a7e37bd64781352b92f4a45cb1cf6ad13775
jpothanc <70871841+jpothanc@users.noreply.github.com>
2025-04-09 11:40:35 +0800

    Update compare_csv.py

--- compare_csv.py
+++ compare_csv.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 def compare_csv_files(file1_path, file2_path, output_path):
-    # Read the CSV files
-    df1 = pd.read_csv(file1_path)
-    df2 = pd.read_csv(file2_path)
+    # Read the CSV files and set the first column as index
+    df1 = pd.read_csv(file1_path, index_col=0)
+    df2 = pd.read_csv(file2_path, index_col=0)
     
     # Find differences between the two dataframes
-    # First, ensure both dataframes have the same columns
+    # First, ensure both dataframes have the same columns (excluding the index)
     columns = list(set(df1.columns) | set(df2.columns))
     
     # Create a dictionary to store different types of changes
@@ -18,26 +18,13 @@ def compare_csv_files(file1_path, file2_path, output_path):
     
     # Find modified rows (rows that exist in both but have different values)
     common_indices = df1.index.intersection(df2.index)
-    changes['modified'] = pd.DataFrame()
-    
     for idx in common_indices:
         if not df1.loc[idx].equals(df2.loc[idx]):
-            # Find which columns changed
-            changed_columns = []
-            changes_details = {}
-            
-            for col in columns:
-                if col in df1.columns and col in df2.columns:
-                    if df1.loc[idx, col] != df2.loc[idx, col]:
-                        changed_columns.append(col)
-                        changes_details[f'{col}_old'] = df1.loc[idx, col]
-                        changes_details[f'{col}_new'] = df2.loc[idx, col]
-            
-            # Create a row with change details
-            change_row = pd.DataFrame([changes_details], index=[idx])
-            change_row['changed_columns'] = ', '.join(changed_columns)
-            change_row['change_type'] = 'modified'
-            changes['modified'] = pd.concat([changes['modified'], change_row])
+            row1 = df1.loc[idx].to_frame().T
+            row2 = df2.loc[idx].to_frame().T
+            row1['change_type'] = 'old_value'
+            row2['change_type'] = 'new_value'
+            changes['modified'] = pd.concat([changes['modified'], row1, row2])
     
     # Add change_type column to added and removed dataframes
     changes['added']['change_type'] = 'added'
@@ -50,8 +37,8 @@ def compare_csv_files(file1_path, file2_path, output_path):
         changes['modified']
     ])
     
-    # Sort the results by index and change_type
-    all_changes = all_changes.sort_values(['change_type', all_changes.index.name or 'index'])
+    # Sort the results by index
+    all_changes = all_changes.sort_index()
     
     # Save the differences to a new CSV file
     all_changes.to_csv(output_path)
